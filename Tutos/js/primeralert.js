0b53795fa46d9e2f56fa551878a56f5bbf99316d
PedroS <pedro.sandoval.martinez@ces-vegamedia.es>
2024-09-20 14:18:30 +0200

    TUtos

---
+++ Tutos/js/primeralert.js
@@ -0,0 +1,16 @@
+const saludar = () =>{
+    alert("Hola gay");
+};
+
+const PPalert = () =>{
+    alert("Esto es una ventana de alerta");
+}
+
+document.write("Hola mundo desde fichero");
+let nombre =prompt("CUal es tu nombre ?","Introduce nombre");
+
+
+let hola=34; 
+
+console.log({hola});
+
--- Tutos/script.js
+++
@@ -1,3 +0,0 @@
-const saludar = () =>{
-    alert("Hola gay");
-};
