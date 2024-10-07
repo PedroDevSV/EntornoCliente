ab8bce3fdb11c674e6f92fe67305195adfc4369b
PedroS <pedro.sandoval.martinez@ces-vegamedia.es>
2024-10-07 10:20:34 +0200

    FUnciones casi terminadas, faltan parentesis

--- 01-Calculadora/assets/script.js
+++ 01-Calculadora/assets/script.js
@@ -12,32 +12,64 @@ const uno     = document.querySelector('#uno'),
       nueve   = document.querySelector('#nueve'), 
       cero    = document.querySelector('#cero');
     
-const suma    = document.querySelector('#suma'),
-      resta   = document.querySelector('#resta'),
-      dividir = document.querySelector('#dividir'),
-      igual   = document.querySelector('#igual'),
-      multi   = document.querySelector('#multi'),
-      punto   = document.querySelector('#punto');
+const operador = [suma    = document.querySelector('#suma'),
+                  resta   = document.querySelector('#resta'),
+                  dividir = document.querySelector('#dividir'),
+                  igual   = document.querySelector('#igual'),
+                  multi   = document.querySelector('#multi')];
+                 
+     
+const punto = document.querySelector('#punto');
 
 let puntosScreen = document.querySelector('#pantalla');
 
+let flagCalculado = false;
 //  FUnciones 
+
 const darValor = (num) => {
     if (puntosScreen.value == '0'){
         puntosScreen.value = num;
     }else{
-        puntosScreen.value += num;
-    } 
-    // Comprobar que no hay operadores
+        if (flagCalculado){
+            puntosScreen.value = num;
+        }else
+            puntosScreen.value += num;
+    }
+
+    // Para operar, que se haya introducido número antes 
+    if (puntosScreen.value != '0'){
+        añadirOperar(operador);
+
+    }else 
+        alert('No se ha introducido ningún número');
+
+   
 };
 
 const borrarTodo = () =>{
     puntosScreen.value = '0';
+    flagCalculado = false;
 }
 
+const añadirOperar = (op) => {
+    // COmprobar qué operador se ha pulsado
+    if (op == '/'){
+        puntosScreen.value += '/';
+    }else if (op == '*'){
+        puntosScreen.value += '*';
+    }else if (op == '+'){
+        puntosScreen.value += '+';
+    }else if (op == '-'){
+        puntosScreen.value += '-';
+    }else if( op == '%'){
+        puntosScreen.value += '%';
+    }
+};
 
-
-
-
-
+const calcular = () =>{
+    if (puntosScreen.value.includes('/') || puntosScreen.value.includes('*') || puntosScreen.value.includes('+') || puntosScreen.value.includes('-') || puntosScreen.value.includes('%')){
+        puntosScreen.value = eval(puntosScreen.value);
+    }
+    flagCalculado = true;
+}
 
